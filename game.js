ab64ab007f7d94120d49623c861728b06cbd947a
PrintTrd <60070034@kmitl.ac.th>
2019-09-29 12:53:46 +0700

    not reset hero's position

--- game.js
+++ game.js
@@ -1,77 +1,75 @@
-/*
-  Code modified from:
-  http://www.lostdecadegames.com/how-to-make-a-simple-html5-canvas-game/
-  using graphics purchased from vectorstock.com
-*/
-// Create the canvas for the game to display in
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 757;
-canvas.height = 710;
+var width = canvas.width = 757;
+var height = canvas.height = 710;
 document.body.appendChild(canvas);
-// Load the background image
+
 var bgReady = false;
 var bgImage = new Image();
 bgImage.onload = function () {
-  // show the background image
   bgReady = true;
 };
 bgImage.src = "images/background.png";
-// Load the hero image
+
 var heroReady = false;
 var heroImage = new Image();
 heroImage.onload = function () {
-  // show the here image
   heroReady = true;
 };
 heroImage.src = "images/hero.png";
-// Load the monster image
+
 var monsterReady = false;
 var monsterImage = new Image();
 monsterImage.onload = function () {
-  // show the monster image
   monsterReady = true;
 };
 monsterImage.src = "images/monster.png";
-// Create the game objects
+
 var hero = {
-  speed: 256 // movement speed of hero in pixels per second
+  speed: 256
 };
 var monster = {};
 var monstersCaught = 0;
-// Handle keyboard controls
+
 var keysDown = {};
-// Check for keys pressed where key represents the keycode captured
+var state = 0;
 addEventListener("keydown", function (key) {
   keysDown[key.keyCode] = true;
 }, false);
 addEventListener("keyup", function (key) {
   delete keysDown[key.keyCode];
 }, false);
-// Reset the player and monster positions when player catches a monster
+
 var reset = function () {
   // Reset player's position to centre of canvas
-  hero.x = canvas.width / 2;
-  hero.y = canvas.height / 2;
+  if (state == 0) {
+    hero.x = width/2;
+    hero.y = height/2;    
+  } else {
+    hero.x = hero.x;
+    hero.y = hero.y;
+  }
   // Place the monster somewhere on the canvas randomly
   monster.x = 32 + (Math.random() * (canvas.width - 64));
   monster.y = 32 + (Math.random() * (canvas.height - 64));
 };
-// Update game objects - change player position based on key pressed
+
 var update = function (modifier) {
-  if (38 in keysDown) { // Player is holding up key
+  // var x = Math.min(Math.max(x,0+20),width-80);
+  // var y = Math.min(Math.max(y,0+80),height-90);  
+  if (38 in keysDown) {
     hero.y -= hero.speed * modifier;
   }
-  if (40 in keysDown) { // Player is holding down key
+  if (40 in keysDown) {
     hero.y += hero.speed * modifier;
   }
-  if (37 in keysDown) { // Player is holding left key
+  if (37 in keysDown) {
     hero.x -= hero.speed * modifier;
   }
-  if (39 in keysDown) { // Player is holding right key
+  if (39 in keysDown) {
     hero.x += hero.speed * modifier;
   }
-  // Check if player and monster collider
+
   if (
     hero.x <= (monster.x + 32)
     && monster.x <= (hero.x + 32)
@@ -79,10 +77,11 @@ var update = function (modifier) {
     && monster.y <= (hero.y + 32)
   ) {
     ++monstersCaught;
+    state = 1;
     reset();
   }
 };
-// Draw everything on the canvas
+
 var render = function () {
   if (bgReady) {
     ctx.drawImage(bgImage, 0, 0);
@@ -93,28 +92,27 @@ var render = function () {
   if (monsterReady) {
     ctx.drawImage(monsterImage, monster.x, monster.y);
   }
-  // Display score and time 
+
   ctx.fillStyle = "rgb(250, 250, 250)";
   ctx.font = "30px Itim";
   ctx.textAlign = "left";
   ctx.textBaseline = "top";
   ctx.fillText("Monsters caught: " + monstersCaught, 20, 20);
   ctx.fillText("Time: " + count, 20, 50);
-  // Display game over message when timer finished
+
   if(finished==true){
-    ctx.fillText("Game over !", 300, 355);
+    ctx.fillText("Game over !", (width/2)-70, height/2);
   }
-  
 };
-var count = 30; // how many seconds the game lasts for - default 30
+
+var count = 30;
 var finished = false;
 var counter =function(){
-  count=count-1; // countown by 1 every second
+  count=count-1;
   // when count reaches 0 clear the timer, hide monster and
   // hero and finish the game
     if (count <= 0)
     {
-      // stop the timer
        clearInterval(counter);
        // set game to finished
        finished = true;
@@ -124,12 +122,12 @@ var counter =function(){
        heroReady=false;
     }
 }
-// timer interval is every second (1000ms)
+
 setInterval(counter, 1000);
 // The main game loop
 var main = function () {
   // run the update function
-  update(0.02); // do not change
+  update(0.02);
   // run the render function
   render();
   // Request to do this again ASAP
